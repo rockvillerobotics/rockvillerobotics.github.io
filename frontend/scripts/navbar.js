4d8b659aed55e3f353e7cd610106a44f1ea2d319
dotcomstar <gamestar109@live.com>
2020-06-23 00:05:41 -0400

    Create basic navbar with JS. It needs CSS still.

--- frontend/scripts/navbar.js
+++ frontend/scripts/navbar.js
@@ -4,9 +4,11 @@ $(function () {
     bar += '<div class="container-fluid">';
     bar += '<div>';
     bar += '<ul class="nav navbar-nav">';
-    bar += '<li id="home"><a href="home.html">Home</a></li>';
-    bar += '<li id="index"><a href="index.html">Index</a></li>';
-    bar += '<li id="about"><a href="about.html">About</a></li>';
+    bar += '<li id="home"><a href="index.html">Home</a></li>';
+    bar += '<li id="code"><a href="https://github.com/RockvilleRobotics/Incredibots2020/">Code</a></li>';
+    bar += '<li id="documentation"><a href="documentation.html">Documentation</a></li>';
+    bar += '<li id="builds"><a href="builds.html">Builds</a></li>';
+    bar += '<li id="videos"><a href="videos.html">Videos</a></li>';
     bar += '</ul>';
     bar += '</div>';
     bar += '</div>';
